fb490ea5fde86cec45cd81bdc7436fe023fe694a
Белецкий Вячеслав <bva@sibdigital.net>
2022-10-28 15:22:26 +0800

    Добавьте файлы проекта.

---
+++ MvvmDemoNet/Converters/IndexToColor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+
+namespace MvvmDemoNet.Converters
+{
+    internal class IndexToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color color = Colors.White;
+            
+                double input = ((Int32)value)/1d;
+                Random random = new Random();
+                
+                byte red = (byte)(random.Next(0, (int)(255 * input / 100d)) );
+                byte green = (byte)(random.Next(0, (int)(255 *input / 100d)));
+                byte blue = (byte)(random.Next(0, (int)(255 *input / 100d)));
+                color = Color.FromRgb(red, green, blue);
+            Brush brush = new SolidColorBrush(color);
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
---
+++ MvvmDemoNet/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDemoNet.ViewModel
+{
+    internal partial class MainViewModel:ObservableObject
+    {
+        public MainViewModel()
+        {
+            indexColor = 10;
+        }
+        [ObservableProperty]
+        int indexColor;
+
+
+    }
+}
